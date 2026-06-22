9ac86a082b9c62b14166671c07134e19a2674958
Nazmi Yılmaz <nazmi.yilmaz@mirket.dev>
2026-06-22 17:47:26 +0300

    change instance count

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -4,7 +4,7 @@ module.exports = {
       name: "fytzone-app",
       port: "4000",
       exec_mode: "cluster",
-      instances: "max",
+      instances: 1,
       script: "./.output/server/index.mjs",
     },
   ],
